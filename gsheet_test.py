56e0bdebaf41520013f032767f9f451a541ac9cc
spitpu <123936515+spitpu@users.noreply.github.com>
2023-01-31 09:42:09 +0800

    Add files via upload

--- gsheet_test.py
+++ gsheet_test.py
@@ -1,22 +1,20 @@
 # streamlit_app.py
 
 import streamlit as st
-from gsheetsdb import connect
+###from gsheetsdb import connect
+from shillelagh.backends.apsw.db import connect
 
 # Create a connection object.
-conn = connect()
+connection = connect(":memory:")
+cursor = connection.cursor()
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
 sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+query = "SELECT * FROM a_table"
 
 # Print results.
-for row in rows:
-    st.write(f"{row.As of date} has a :{row.Number of confirmed cases}:")
+for row in cursor.execute(query):
+    print(row)
+
